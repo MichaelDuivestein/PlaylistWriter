992478fadc3b019efa32f33604ba77e10914e448
michaelduivestein <michael.duivestein+github@gmail.com>
2018-05-10 22:03:58 +0200

    Added JAudioTagger for tagging

--- src/za/co/k0ma/playlistWriter/PlaylistWriter.kt
+++ src/za/co/k0ma/playlistWriter/PlaylistWriter.kt
@@ -1,5 +1,5 @@
+import za.co.k0ma.playlistWriter.readTag
 import java.io.File
-import java.io.RandomAccessFile
 import java.util.*
 
 /**
@@ -15,6 +15,7 @@ const val PLAYLIST_HEADER = "#EXTM3U"
 const val ENTRY_START = "#EXTINF:"
 
 class PlaylistWriter(private val outputPathAndName: String) {
+	companion object TagReader
 	
 	/**
 	 * Basic shuffle. Can do something better later
@@ -47,106 +48,12 @@ class PlaylistWriter(private val outputPathAndName: String) {
 		}
 	}
 	
-	private fun createTitle(file: File, readTags: Boolean) =
-			if (readTags) {
-				readTag(file)
-			} else {
-				file.nameWithoutExtension
-			}
-	
-	private fun readTag(file: File) = when (file.extension.toLowerCase()) {
-		"mp3" -> readID3Tag(file)
-		else -> {
-			file.nameWithoutExtension
+	private fun createTitle(file: File, readTags: Boolean): String {
+		if (readTags) {
+			return readTag(file)
+		} else {
+			return file.nameWithoutExtension
 		}
 	}
-	
-	//TODO: Move tag reading stuff to it's own class
-	private fun readID3Tag(file: File): String {
-		val randomAccessFile = RandomAccessFile(file, "r")
-		try {
-			return when (getTagType(randomAccessFile)) {
-				TagType.NONE -> file.nameWithoutExtension
-				TagType.ID3V1 -> readID3V1Tag(randomAccessFile)
-				TagType.ID3V1_ENHANCED -> readID3V1EnhancedTag(randomAccessFile)
-				TagType.ID3V2 -> readID3V2Tag(randomAccessFile, file.nameWithoutExtension)
-			}
-		} catch (exception: Exception) {
-			println("ERROR: ")
-			println(exception.stackTrace)
-		} finally {
-			randomAccessFile.close()
-		}
-		
-		return file.nameWithoutExtension
-	}
-	
-	private fun readBytes(randomAccessFile: RandomAccessFile, length: Int, stripWhitespace: Boolean): String {
-		val tagBytes = ByteArray(length)
-		randomAccessFile.read(tagBytes)
-		val value = String(tagBytes, Charsets.UTF_8)
-		
-		return if (stripWhitespace) value.replace("\u0000", "").trim() else value
-	}
-	
-	//Checks for ID3 Tag and returns version.
-	/**
-	 *  @return NONE if no tag, otherwise tag version
-	 */
-	private fun getTagType(randomAccessFile: RandomAccessFile): TagType {
-		
-		//TODO: let's try with the most common: ID3V2. (Some ID3V2 files contain a 'backup' ID3V1)
-		
-		//perhaps it's an ID3v1 tag
-		randomAccessFile.seek(randomAccessFile.length() - 128)
-		var tag = readBytes(randomAccessFile, 3, true)
-		if ("tag".equals(tag, true)) {
-			//There is no ID3 tag
-			return TagType.ID3V1
-		}
-		
-		//see if it's an ID3V1Enhanced tag
-		//do this last, as these tags are hardly used and often stale.
-		randomAccessFile.seek(randomAccessFile.length() - 227)
-		tag = readBytes(randomAccessFile, 4, true)
-		if ("tag+".equals(tag, true)) {
-			//There is no ID3 tag
-			return TagType.ID3V1_ENHANCED
-		}
-		
-		return TagType.NONE
-	}
-	
-	private fun readID3V1Tag(randomAccessFile: RandomAccessFile) = readID3V1Tag(randomAccessFile, 125, 30)
-	private fun readID3V1EnhancedTag(randomAccessFile: RandomAccessFile) = readID3V1Tag(randomAccessFile, 224, 60)
-	
-	private fun readID3V1Tag(randomAccessFile: RandomAccessFile, seekDistanceFromEnd: Int, elementLength: Int): String {
-		randomAccessFile.seek(randomAccessFile.length() - seekDistanceFromEnd)
-		
-		//read artist
-		var title = readBytes(randomAccessFile, elementLength, true).takeIf { it.isNotBlank() } ?: "Unknown Title"
-		
-		//read title
-		var artist = readBytes(randomAccessFile, elementLength, true).takeIf { it.isNotBlank() } ?: "Unknown Artist"
-		
-		return "$artist - $title"
-	}
-	
-	private fun readID3V2Tag(randomAccessFile: RandomAccessFile, tempFileName: String): String {
-		return tempFileName
-		//TODO:
-		//https://github.com/mpatric/mp3agic
-	}
-	
-	enum class TagType {
-		NONE,
-		ID3V1,
-		ID3V1_ENHANCED,
-		ID3V2
-	}
-	
-	//read ID3 tags: https://revbingo.github.io/kotlin/2017/06/25/kotlin-refactoring-dsl.html
-	// ID3 for MP3, WMA (modded ID3), https://en.wikipedia.org/wiki/ID3
-	//Vorbis comment for ogg, FLAC: https://en.wikipedia.org/wiki/Vorbis_comment
 }
 
---
+++ src/za/co/k0ma/playlistWriter/TagReader.kt
@@ -0,0 +1,52 @@
+package za.co.k0ma.playlistWriter
+
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.audio.exceptions.CannotReadException
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException
+import org.jaudiotagger.tag.FieldKey
+import java.io.File
+
+
+/**
+ * Attempts to read any tag in the file
+ */
+fun readTag(file: File) = when (file.extension.toLowerCase()) {
+	"flac" -> readTagWithAudioFileIO(file)
+	"mp3" -> readTagWithAudioFileIO(file)
+	"mp4" -> readTagWithAudioFileIO(file)
+	"m4a" -> readTagWithAudioFileIO(file)
+	"ogg" -> readTagWithAudioFileIO(file)
+	"wav" -> readTagWithAudioFileIO(file)
+	"wma" -> readTagWithAudioFileIO(file)
+	else -> {
+		file.nameWithoutExtension
+	}
+}
+
+//TODO: this is very slow. In future maybe look at faster implementations for reading tags.
+fun readTagWithAudioFileIO(file: File): String {
+	var artist = "Unknown Artist"
+	var title = "Unknown Title"
+	val unknownData = "Unknown Artist - Unknown Title"
+	try {
+		val audioFile = AudioFileIO.read(file)
+		//sometimes a file will not have a tag
+		if (audioFile == null || audioFile.tag == null) {
+			return file.name
+		}
+		artist = audioFile.tag.getFirst(FieldKey.ARTIST).takeIf { (it != null) && it.isNotBlank() } ?: artist
+		title = audioFile.tag.getFirst(FieldKey.TITLE).takeIf { (it != null) && it.isNotBlank() } ?: title
+		
+	} catch (ex: Exception) {
+		when (ex) {
+			is CannotReadException,
+			is InvalidAudioFrameException -> {
+				//sometimes files don't contain valid tag data. Log and continue.
+				println(ex)
+			}
+			else -> println(ex)
+		}
+		
+	}
+	return "$artist - $title".takeIf { ! it.equals(unknownData) } ?: file.name
+}
